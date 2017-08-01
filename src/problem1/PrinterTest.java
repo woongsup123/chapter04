ea2cd6c43c86e59a4da7f379d43a3b5b3eb2d9ab
bit-user <bit-user@DESKTOP-CNKQU76>
2017-08-01 17:05:22 +0900

    init commit // complete problem1

---
+++ src/problem1/PrinterTest.java
@@ -0,0 +1,10 @@
+package problem1;
+public class PrinterTest {
+	public static void main(String[] args) {
+		Printer printer = new Printer();
+		printer.println( 10 );
+		printer.println( true );
+		printer.println( 5.7 );
+		printer.println( "홍길동" );
+	}
+}
