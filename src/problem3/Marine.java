b648a2f97600507a6beea7e3a314f2eee40b14f9
bit-user <bit-user@DESKTOP-CNKQU76>
2017-08-03 16:05:41 +0900

    Create and implement parent class Unit.java

---
+++ src/problem3/Marine.java
@@ -0,0 +1,8 @@
+package problem3;
+
+public class Marine  extends Unit {
+	
+	void stimPack() { 
+		/* 스팀팩을 사용한다.*/
+	}	
+}
---
+++ src/problem3/Tank.java
@@ -0,0 +1,8 @@
+package problem3;
+
+public class Tank  extends Unit {
+	
+	void changeMode() {
+		/* 공격모드를 변환한다. */
+	}	
+}
